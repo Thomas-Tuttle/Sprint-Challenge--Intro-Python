27f47f18a4bbe6b6aba81d3e654e1439f70d58ed
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-09-06 11:28:48 -0400

    Vehichle sounds in oop2 and tests run for oop1 and 2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Base Class
+class Vehicle:
+    def __init__(self):
+        pass
+
+#Sub-class of Vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+# Sub-class of Vehicle
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+#Sub-class of FlightVehicle
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
+
+#Sub-class of FlightVehicle
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+#Sub-class of GroundVehicle
+class Car(GroundVehicle): 
+    def __init__(self):
+        pass
+
+#Sub-class of GroundVehicle
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
